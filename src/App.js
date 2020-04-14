e37423269691cda33a814f8667533c79d5e2feb5
Konstantin <k.podyganov@mail.ru>
2020-04-14 19:48:35 +0300

    react initialization

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import { IndexPage } from './pages/IndexPage';
-import { AboutPage } from './pages/AboutPage';
+import { Message } from './components/Message';
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path='/about' component={AboutPage} />
-        <Route path='/' component={IndexPage} />
-      </Switch>
-    </BrowserRouter>
-  );
+  return <Message />;
 }
