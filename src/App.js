888219e3433a34d85453a6636347974c3e391c61
Konstantin <k.podyganov@mail.ru>
2020-04-28 20:25:20 +0300

    Moved to functions from classes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,6 @@ import React from 'react';
 
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return <div></div>;
-  }
+export default function App() {
+  return <div className='app'></div>;
 }
-
-export default App;
